ce147e9aaa87a433cb66a950f1b19b61f0dfc6c1
eakyoungyu <eakyoungyu@gmail.com>
2018-06-06 00:33:39 +0900

    ResevationTime 변경

--- app/src/main/java/org/project/softwareproject_2018/ReservationTime.java
+++ app/src/main/java/org/project/softwareproject_2018/ReservationTime.java
@@ -7,17 +7,13 @@ package org.project.softwareproject_2018;
 public class ReservationTime {
     protected String cid;
     protected String tid;
-    protected int year;
-    protected int month;
-    protected int dayOfMonth;
-    protected int startTime;
+    protected String date;
+    protected String startTime;
     public ReservationTime(){}
-    public ReservationTime(String cid, String tid, int year, int month, int dayOfMonth, int startTime){
+    public ReservationTime(String cid, String tid, String date, String startTime){
         this.cid=cid;
         this.tid=tid;
-        this.year=year;
-        this.month=month;
-        this.dayOfMonth=dayOfMonth;
+        this.date=date;
         this.startTime=startTime;
     }
 }
