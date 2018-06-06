da516fa6079f3167f12c38b62d67a165862788e4
ssiyeong <39458505+ssiyeong@users.noreply.github.com>
2018-06-06 20:04:38 +0900

    트레이너 메인화면 달력 기능 추가

--- app/src/main/java/org/project/softwareproject_2018/trainerMainActivity.java
+++ app/src/main/java/org/project/softwareproject_2018/trainerMainActivity.java
@@ -13,16 +13,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class trainerMainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private TextView nameTextView;
     private CalendarView calendarView;
+    private ArrayList<HashMap<String, String>> items = new ArrayList<HashMap<String, String>>() ;
+    private HashMap<String, String> item;
+    private SimpleAdapter adapter;
 
     private FirebaseAuth auth;
 
@@ -45,13 +54,29 @@ public class trainerMainActivity extends AppCompatActivity
         nameTextView = (TextView)view.findViewById(R.id.header_name_textview);
         nameTextView.setText(auth.getCurrentUser().getEmail()+"님");
 
+        // listview 생성 및 adapter 지정.
+        final ListView listview = (ListView) findViewById(R.id.listview) ;
+        adapter = new SimpleAdapter(this, items, android.R.layout.simple_list_item_2,
+                new String[]{"time", "customer"},
+                new int[]{android.R.id.text1, android.R.id.text2}) ;
+        listview.setAdapter(adapter) ;
+
         calendarView = (CalendarView) findViewById(R.id.calendar);
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                //Arraylist 초기화
+                items.clear();
 
+                // 아이템 추가 예
+                item = new HashMap<String, String>();
+                item.put("time", "2");
+                item.put("customer", "2");
+                items.add(item);
+
+                // listview 갱신
+                adapter.notifyDataSetChanged();
             }
         });
-
 
     }
 
